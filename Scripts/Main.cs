51785ab92b36330ebd0fa934e0a7d308f7bbfd36
murchalkin <106840199+murchalkin@users.noreply.github.com>
2022-10-15 19:20:32 +0300

    ok

---
+++ Scripts/Main.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace HTTPUnityLib
+{
+    public static class Server : MonoBehaviour
+    {
+      // string text = Server.DownloadText("url");
+      // Download Text and return value;
+      public static string DownloadText(string url)
+      {
+         
+      }
+    }
+}
